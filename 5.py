8e3e5361123bb7ca24f921aa6c62dcb9acc04438
piyush-madda <72153460+piyush-madda@users.noreply.github.com>
2020-10-15 13:58:50 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,9 +1,12 @@
-n = int(input())
-arr = list(map(int, input().split()))
-zes = max(arr)
-i=0
-while(i<n):
-    if zes ==max(arr):
-        arr.remove(max(arr))
-    i+=1
-print(max(arr))
+def sec_max_finder(n,arr):
+    zes = max(arr)
+    i=0
+    while(i<n):
+        if zes ==max(arr):
+            arr.remove(max(arr))
+        i+=1
+    return max(arr)
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(sec_max_finder(n,arr))
